20a89ef01b61bd8996b7e836e207a87a9c5665ad
sunchenghui <j-sunchenghui-tc@360ityun.com>
2021-09-28 15:30:08 +0800

    e

--- b.php
+++ b.php
@@ -1,4 +1,4 @@
-<?p
+<?php
 
 namespace App\Services;
 
@@ -8,7 +8,7 @@ use App\Wheel\TcbCloudStorage\CloudStorageFactory;
  * Class PicassoServer
  * @package App\Services
  */
-//class PicassoServer
+class PicassoServer
 {
     private $obj;
     /**
