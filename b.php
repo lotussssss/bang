d8fa8be56c4339fc35530ca5ff4d465d3b4b9676
sunchenghui <j-sunchenghui-tc@360ityun.com>
2021-09-28 15:34:02 +0800

    e

--- b.php
+++ b.php
@@ -1,42 +1,5 @@
 <?php
+    echo "ask";
+?>
 
-namespace App\Services;
-
-use App\Wheel\TcbCloudStorage\CloudStorageFactory;
-
-/**
- * Class PicassoServer
- * @package App\Services
- */
-class PicassoServer
-{
-    private $obj;
-    /**
-     * @var PicassoClient
-     */
-    private $client;
-
-    /**
-     * PicassoServer constructor.
-     */
-    public function __construct()
-    {
-
-        $this->obj    = CloudStorageFactory::createCloudObj();
-        $this->client = $this->obj->getClient();
-    }
-
-    /**
-     * @return PicassoClient
-     */
-    public function getClient()
-    {
-        return $this->client;
-    }
-
-    public function __call($name, $arguments)
-    {
-        return call_user_func_array([$this->obj, $name], $arguments);
-    }
-}
 
